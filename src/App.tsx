8596b57d15188faf75656fca3ebb28c8a5fe846e
iamidlek <iamidlek@naver.com>
2021-12-23 00:39:55 +0900

    드레그 가능하게하는 props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function App() {
+  // 드래그가 끝나면 실행할 내용
   const onDragEnd = () => {};
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    {/* 핸들을 주면 해당태그 영역으로만 드래그 가능 */}
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    One
+                  </li>
+                )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.dragHandleProps}
+                    {...magic.draggableProps}
+                  >
+                    Two
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
